04fa9705b115681478ae4eb719c2466ce36b42d8
Nadno <eunadno@gmail.com>
2021-02-15 15:59:03 -0300

    added Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../src/components/Header';
 
 const IndexPage = () => (
-  <div>
-
-  </div>
+  <>
+    <Header />
+  </>
 )
 
 export default IndexPage

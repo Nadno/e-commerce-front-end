247ae6eedaba0c34819e1ba9882f0142a4b063c0
Nadno <eunadno@gmail.com>
2021-02-20 12:38:28 -0300

    component and style, created Input Field

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-const Input = styled.input`
+const Input: any = styled.input`
   width: 100%;
   height: 4rem;
-  border: 2px solid ${({theme})=>theme.colors.shadow};
+  border: 2px solid ${({ theme }) => theme.colors.shadow};
   border-radius: 0.5rem;
   background-color: #fff;
 
@@ -12,9 +12,34 @@ const Input = styled.input`
   box-shadow: 0 8px 16px ${({ theme }) => theme.colors.shadow};
 
   font-size: 1.6rem;
-  color: ${({theme})=>theme.colors.text};
+  color: ${({ theme }) => theme.colors.text};
 
   &:focus {
     outline: none;
   }
 `;
+
+Input.Field = styled.div`
+  position: relative;
+
+  &:focus-within::after {
+    content: '';
+
+    position: absolute;
+    width: calc(100% - 4px);
+    height: 0.2rem;
+
+    border-radius: 0 0 0.5rem 0.5rem;
+    background-color: #83b636;
+
+    bottom: 0.2rem;
+    left: 0;
+    transform: translateX(2px);
+  }
+
+  &.invalid::after {
+    background-color: red;
+  }
+`;
+
+export default Input;

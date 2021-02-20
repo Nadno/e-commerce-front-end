e25088c1c92919b6529d9f9d0db201491c7f9b2a
Nadno <eunadno@gmail.com>
2021-02-20 10:14:34 -0300

    style, added top border, and responsiveness

--- src/components/Product/style.ts
+++ src/components/Product/style.ts
@@ -1,42 +1,57 @@
 import styled from 'styled-components';
 
-const StyledProduct = styled.li`
-  width: 28rem;
+const StyledProduct: any = styled.div`
+  width: 100%;
 
+  padding: 1.5rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
 
+  box-shadow: 
+    0 -4px 0 ${({theme}) => theme.colors.primary},
+    0 0 16px 8px ${({ theme }) => theme.colors.shadow};
+  background-color: white;
+
   gap: 2.5rem;
-  padding: 2rem;
-  border-radius: 1rem;
-  box-shadow: 0px 0px 8px 1px ${({ theme }) => theme.colors.shadow};
-  background-color: ${({ theme }) => theme.colors.white};
 
   .img {
     width: 100%;
     object-fit: cover;
     margin-bottom: 2rem;
   }
+`;
+
+StyledProduct.Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  gap: 2rem;
 
   .info {
-    width: 100%;
-    font-size: 1.4rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
 
   .title {
     font-size: 2rem;
   }
 
+  .description {
+    height: 100%;
+    display: block;
+    font-size: 1.6rem;
+  }
+
   .price {
     font-size: 2.4rem;
     font-weight: bold;
     color: ${({ theme }) => theme.colors.secondary};
-  }
-
-  .more {
-    margin-top: 2rem;
   }
 `;
 

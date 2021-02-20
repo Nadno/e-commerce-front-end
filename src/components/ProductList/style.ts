28b64ad78e58ba905324bc268567d5f633393c0b
Nadno <eunadno@gmail.com>
2021-02-20 10:10:08 -0300

    style, ProductList extend Container styles and added responsiveness

--- src/components/ProductList/style.ts
+++ src/components/ProductList/style.ts
@@ -1,19 +1,27 @@
 import styled from 'styled-components';
+import Container from '../Container';
 
-const StyledProductList = styled.section`
+const StyledProductList = styled(Container)`
   width: 100%;
-  margin-top: 8rem;
-  padding: 0 4rem;
+  padding: 4rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   .title {
     font-size: 2.4rem;
     color: ${({ theme }) => theme.colors.title};
+    margin: 2rem 0;
   }
 
   .list {
+    width: 100%;
     list-style: none;
     padding-top: 4rem;
     display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 24rem));
+    justify-content: center;
     gap: 2rem;
   }
 `;

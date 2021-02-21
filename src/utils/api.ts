094447c15be6ce8c50b9af06b167d5d1dc53da61
Nadno <eunadno@gmail.com>
2021-02-21 04:09:18 -0300

    util, created getHeaders and apiPost

---
+++ src/utils/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import { getCookie } from './storage';
+import { COOKIE_TOKEN } from './account';
+
+export const getApiUrl = (path: string) => `http://localhost:3333${path}`;
+
+const getHeaders = () => {
+  const token = getCookie(COOKIE_TOKEN);
+  if (!token) return {};
+
+  return {
+    Authorization: `Bearer ${token}`,
+  }
+}
+
+export const apiPost = (path: string, data = {}) => {
+  const url = getApiUrl(path);
+  const options = {
+    headers: getHeaders(),
+  };
+
+  return axios.post(url, data, options);
+}

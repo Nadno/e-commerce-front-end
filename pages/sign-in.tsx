8158c7ca30e6a5bab9daaf0533f151e8444b7795
Nadno <eunadno@gmail.com>
2021-02-21 01:09:36 -0300

    component, created SignIn and using FormData HOC to form data

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useCallback, FormEvent } from 'react';
+import Form from '../src/components/Form';
 
-const SignIn: React.FC = () => {
-  return (
-    <form>
-      
-    </form>
-  );
-};
+import Input from '../src/components/Input';
+import FormData from '../src/HOC/form';
+
+const SignIn = FormData(
+  ({ data, handleChange }) => {
+    const handleSubmit = useCallback((e: FormEvent) => {
+      e.preventDefault();
+    }, []);
+
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Fieldset>
+          <legend>Bem vindo!</legend>
+          <Input
+            id="email"
+            name="email"
+            value={data.email}
+            label="Email"
+            placeholder="Digite seu e-mail"
+            handleChange={handleChange}
+          />
+
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            value={data.password}
+            label="Senha"
+            placeholder="Digite sua senha"
+            handleChange={handleChange}
+          />
+
+          <Form.Submit>Login</Form.Submit>
+        </Form.Fieldset>
+      </Form>
+    );
+  },
+  { email: '', password: '' }
+);
 
 export default SignIn;

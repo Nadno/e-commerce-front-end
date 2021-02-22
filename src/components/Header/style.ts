16eb4ffd441e3e55906d0d12e36e943902a013a4
Nadno <eunadno@gmail.com>
2021-02-22 10:53:17 -0300

    style Header, styled links and transition on hover effect

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,9 +4,32 @@ import Container from '../Container';
 const StyledHeader = styled(Container)`
   width: 100%;
   height: 5rem;
+  padding: 0;
 
   nav {
     height: 100%;
+  }
+
+  li {
+    cursor: pointer;
+    min-width: 10rem;
+    height: 100%;
+
+    a {
+      height: 100%;
+      text-decoration: none;
+      color: ${({ theme }) => theme.colors.title};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      transition: background-color 0.5s ease;
+
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.secondary};
+        color: ${({ theme }) => theme.colors.white};
+      }
+    }
   }
 
   .list {

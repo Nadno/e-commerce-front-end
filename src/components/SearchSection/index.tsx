f796d56d90a1e4ab1fde49a0f691019ca7e2285c
Nadno <eunadno@gmail.com>
2021-02-22 15:34:13 -0300

    component SearchSection, excluded useRef and implemented form and Submit button

--- src/components/SearchSection/index.tsx
+++ src/components/SearchSection/index.tsx
@@ -1,7 +1,7 @@
 import React, {
   ChangeEvent,
+  FormEvent,
   useCallback,
-  useRef,
   useState,
 } from 'react';
 
@@ -10,7 +10,7 @@ import { apiGet } from '../../utils/api';
 
 import { StyledInput } from '../Input';
 
-import * as Style from './style';
+import * as Search from './style';
 
 interface Props {
   categories: string[];
@@ -19,7 +19,6 @@ interface Props {
 
 const SearchSection: React.FC<Props> = ({ setProducts, categories }) => {
   const [productName, setProductName] = useState('');
-  const searchBar = useRef<HTMLInputElement>(null);
 
   const searchChange = useCallback(
     (e: ChangeEvent) => {
@@ -29,29 +28,34 @@ const SearchSection: React.FC<Props> = ({ setProducts, categories }) => {
     [productName]
   );
 
-  const handleSearch = useCallback(async () => {
-    if (searchBar.current) {
-      const input = searchBar.current;
-      const products = await apiGet(`/product/name?value=${input.value}`).then(
+  const handleSearch = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+      const products = await apiGet(`/product/name?value=${productName}`).then(
         ({ data }) => data.products || data.product
       );
       setProducts(products);
-    }
-  }, [productName]);
+    },
+    [productName]
+  );
 
   return (
-    <Style.Section>
-      <div className="search-bar">
+    <Search.Section>
+      <form className="search-bar" onSubmit={handleSearch}>
         <StyledInput
           id="search"
           name="search"
           value={productName}
           onChange={searchChange}
-          ref={searchBar}
         />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-    </Style.Section>
+        <Search.Submit
+          as="button"
+          type="submit"
+        >
+          Search
+        </Search.Submit>
+      </form>
+    </Search.Section>
   );
 };
 

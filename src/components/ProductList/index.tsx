9c48328493fd9aa156411be577c1f1573fb9eaa8
Nadno <eunadno@gmail.com>
2021-02-22 10:58:22 -0300

    component ProductList, implemented axios request and handle request and conditional rendering

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -3,33 +3,46 @@ import IProduct from '../../interfaces/product';
 import Product from '../Product';
 import StyledProductList from './style';
 
+import { apiGet } from '../../utils/api';
+import handleRequest from '../../utils/handleRequests';
 interface Props {
-  category?: string;
+  category?: number;
 }
 
 const ProductList: React.FC<Props> = ({ category }) => {
   const [products, setProducts] = useState<Array<IProduct>>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    const URL = 'http://localhost:3000/api/db';
+    const url = (category || -1) >= 0
+      ? `/product/category?value=${category}`
+      : '/product';
 
-    fetch(URL)
-      .then((res) => res.json())
-      .then(items => {
-        console.log(items);
-        setProducts(items)
-      });
+    apiGet(url)
+      .then(({ data }) => {
+        setProducts(data.products);
+      })
+      .catch(handleRequest(setError));
   }, []);
 
   return (
     <StyledProductList>
-      <h2 className="title">Produtos</h2>
-      <ul className="list">
-        {products.length > 0 &&
-          products.map((product) => 
-            <Product {...product} key={product.id} />
-          )}
-      </ul>
+      {products.length > 0 ? (
+        <>
+          <h2 className="title">Produtos</h2>
+          <ul className="list">
+            {products.length > 0 &&
+              products.map((product) => (
+                <Product {...product} key={product.id} />
+              ))}
+          </ul>
+        </>
+      ) : (
+        <>
+          <div className="nothing">Nada encontrado!</div>
+          {error && <div className="error">{error}</div>}
+        </>
+      )}
     </StyledProductList>
   );
 };

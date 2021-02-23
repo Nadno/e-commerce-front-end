7092d2810ab8c38864a04f006610eca99e0bf241
Nadno <eunadno@gmail.com>
2021-02-23 14:08:59 -0300

    component SearchSection, changed body structure of Container

--- src/components/SearchSection/index.tsx
+++ src/components/SearchSection/index.tsx
@@ -1,16 +1,12 @@
-import React, {
-  ChangeEvent,
-  FormEvent,
-  useCallback,
-  useState,
-} from 'react';
+import React, { ChangeEvent, FormEvent, useCallback, useState } from 'react';
 
 import Link from '../Link';
 import { apiGet } from '../../utils/api';
 
-import { StyledInput } from '../Input';
+import { Input } from '../Input';
 
 import * as Search from './style';
+import Container from '../Container';
 
 interface Props {
   categories: string[];
@@ -31,38 +27,38 @@ const SearchSection: React.FC<Props> = ({ setProducts, categories }) => {
   const handleSearch = useCallback(
     async (e: FormEvent) => {
       e.preventDefault();
+
       const products = await apiGet(`/product/name?value=${productName}`).then(
         ({ data }) => data.products || data.product
       );
-      setProducts(products);
+      setProducts(() => products);
     },
     [productName]
   );
 
   return (
     <Search.Section>
-      <form className="search-bar" onSubmit={handleSearch}>
-        <StyledInput
-          id="search"
-          name="search"
-          value={productName}
-          onChange={searchChange}
-        />
-        <Search.Submit
-          as="button"
-          type="submit"
-        >
-          Search
-        </Search.Submit>
-      </form>
+      <Container title="Pesquise">
+        <form className="search-bar" onSubmit={handleSearch}>
+          <Input
+            id="search"
+            name="search"
+            value={productName}
+            onChange={searchChange}
+          />
+          <Search.Submit as="button" type="submit">
+            Search
+          </Search.Submit>
+        </form>
 
-      <ul className="categories">
-        {categories.map((cat) => (
-          <li key={cat}>
-            <Link href={`/products/category/${cat}`}>{cat}</Link>
-          </li>
-        ))}
-      </ul>
+        <ul className="categories">
+          {categories.map((cat) => (
+            <li key={cat}>
+              <Link href={`/products/category/${cat}`}>{cat}</Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
     </Search.Section>
   );
 };

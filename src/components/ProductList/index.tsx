2c6fa560e9def84d563b8faaa9319abf844fb333
Nadno <eunadno@gmail.com>
2021-02-23 14:05:26 -0300

    component ProductList, changed component body structure

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+
+import Container from '../Container';
 import IProduct from '../../interfaces/product';
 import Product from '../Product';
-import StyledProductList from './style';
+import Section from './style';
+import Button from '../Button';
+
 interface Props {
   error?: string;
   products: IProduct[];
@@ -9,24 +13,27 @@ interface Props {
 
 const ProductList: React.FC<Props> = ({ error, products }) => {
   return (
-    <StyledProductList>
-      {products.length > 0 ? (
-        <>
-          <h2 className="title">Produtos</h2>
+    <Section>
+      <Container title="Produtos">
+        {products.length > 0 ? (
           <ul className="list">
             {products.length > 0 &&
               products.map((product) => (
-                <Product {...product} key={product.id} />
+                <Product {...product} key={product.id}>
+                  <Button.Link href={`/products/${product.id}`}>
+                    Detalhes
+                  </Button.Link>
+                </Product>
               ))}
           </ul>
-        </>
-      ) : (
-        <>
-          <div className="nothing">Nada encontrado!</div>
-          {error && <div className="error">{error}</div>}
-        </>
-      )}
-    </StyledProductList>
+        ) : (
+          <>
+            <div className="nothing">Nada encontrado!</div>
+            {error && <div className="error">{error}</div>}
+          </>
+        )}
+      </Container>
+    </Section>
   );
 };
 

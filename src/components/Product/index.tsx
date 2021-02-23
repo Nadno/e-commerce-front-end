224c1d283fd634b06fef15dad0b65432b98beacc
Nadno <eunadno@gmail.com>
2021-02-23 14:03:34 -0300

    component Product, changed imports names and removed Content component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
-import IProduct from '../../interfaces/product';
 import formatCurrency from '../../utils/formatCurrency';
 import formatDescription from '../../utils/formatDescription';
-import Button from '../Button';
-import StyledProduct from './style';
+import ListItem from './style';
 
-const Product: React.FC<IProduct> = ({
+import ProductItem from '../../interfaces/product';
+
+interface Props extends ProductItem {
+  type?: 'normal' | 'cart';
+}
+
+const Product: React.FC<Props> = ({
   description,
   id,
   image,
   price,
   title,
+  type = 'normal',
+  children,
 }) => {
   return (
-    <StyledProduct as="li" title={title}>
+    <ListItem title={title}>
       <img src={image} alt={title} className="img" />
 
-      <StyledProduct.Content>
+      <div className={`content ${type}`}>
         <div className="info">
           <h3 className="title">{title}</h3>
-          <p className="description" title={description}>{formatDescription(description)}</p>
+          <p className="description" title={description}>
+            {formatDescription(description)}
+          </p>
           <span className="price">{formatCurrency(price)}</span>
         </div>
 
-        <Button.Link href={`/products/${id}`}>Detalhes</Button.Link>
-      </StyledProduct.Content>
-    </StyledProduct>
+        <div className="buttons">
+          {children}
+        </div>
+      </div>
+    </ListItem>
   );
 };
 

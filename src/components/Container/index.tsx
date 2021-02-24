511d05a2ea5dc718e838cc78e22d079c73754808
Nadno <eunadno@gmail.com>
2021-02-24 15:58:22 -0300

    component, added backTo option Container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import Button from '../Button';
 import Container from './style';
 
 interface Props {
   title: string;
+  backTo?: string;
 }
 
-const Section: React.FC<Props> = ({ title, children }) => {
+const Section: React.FC<Props> = ({ title, backTo, children }) => {
   return (
     <Container className="container">
-      <h2 className="title">{title}</h2>
+      {backTo ? (
+        <div className="header">
+          <h2 className="title">{title}</h2>
+          <Button.Back className="back-to" />
+        </div>
+      ) : (
+        <h2 className="title">{title}</h2>
+      )}
       {children}
     </Container>
-  )
-}
+  );
+};
 
 export default Section;

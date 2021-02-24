56ed63c55c3a9ead0c7aa72d863cc4168e816998
Nadno <eunadno@gmail.com>
2021-02-24 17:20:43 -0300

    hook Cart, fixed removeFromCart and added setCookie to save cart

--- src/Hooks/useCart.ts
+++ src/Hooks/useCart.ts
@@ -1,7 +1,10 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { ContextAccount } from '../StoreProvider';
 
-import { CartAction } from '../hooks';
+import { CartAction } from '../interfaces/hooks';
+import { setCookie, getCookie } from '../utils/storage';
+
+const COOKIE_CART = 'cart';
 
 const useCart = () => {
   const ctx = useContext(ContextAccount);
@@ -9,19 +12,47 @@ const useCart = () => {
 
   const { cart, setCart } = ctx;
 
+  useEffect(() => {
+    const storageCart = getCookie(COOKIE_CART);
+    if (storageCart) setCart(storageCart);
+  }, []);
+
   const addToCart: CartAction = (id) => {
+    let error = false;
+
     setCart((cart) => {
-      if (cart.includes(id)) return cart;
-      return [...cart, id];
+      if (cart.includes(id)) {
+        error = true;
+        return cart;
+      }
+
+      const newCart = [...cart, id];
+      setCookie(COOKIE_CART, newCart);
+
+      return newCart;
     });
+
+    return error;
   };
 
   const removeFromCart: CartAction = (id) => {
+    let error = false;
+
     setCart((cart) => {
       const indexId = cart.indexOf(id);
+      const items = cart.slice();
 
-      return ~indexId ? cart.slice(indexId, 1) : cart;
+      if (~indexId) {
+        items.splice(indexId, 1);
+        setCookie(COOKIE_CART, items);
+        return items;
+      }
+
+      error = true;
+      return items;
     });
+
+    return error;
   };
 
   return {

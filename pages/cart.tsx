32051dfd9461b3b521d8df98ffa1f718cb43ac15
Nadno <eunadno@gmail.com>
2021-02-25 10:12:47 -0300

    page cart, imported useAccount, Model and its ref

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import useAccount from '../src/hooks/useAccount';
+import useCart from '../src/hooks/useCart';
 
 import Cart from '../src/components/Cart';
 import Header from '../src/components/Header';
-
+import Modal, { ModalHandle } from '../src/components/Modal';
 
 const CartPage: React.FC = () => {
+  const { token, secondsToExpire } = useAccount();
+  const { cart, removeFromCart } = useCart();
+  const [step, setStep] = useState(0);
+
+  const modalRef = useRef<ModalHandle>(null);
+
+  const submitStep = useCallback(() => {
+    if (secondsToExpire(token) < 1) {
+
+    } else {
+      setStep((prev) => prev + 1);
+    }
+  }, []);
+
   return (
     <>
       <Header />
-      <Cart items={['1', '2']} />
+      <Cart items={cart} removeItem={removeFromCart} submitStep={submitStep} />
+      <Modal ref={modalRef} />
     </>
   );
-}
+};
 
 export default CartPage;

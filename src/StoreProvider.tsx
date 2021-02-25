1300f34d0791a5887162a2753f76082a33aa302b
Nadno <eunadno@gmail.com>
2021-02-25 12:05:49 -0300

    component StoreProvider, implemented authorized state, reimplemented loginByStorage and crated startCountDown to set authorized to false when token expire

--- src/StoreProvider.tsx
+++ src/StoreProvider.tsx
@@ -1,9 +1,8 @@
-import React, {
-  createContext,
-  useState,
-} from 'react';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
 
 import { StoreProvider, Account } from './interfaces/hooks';
+import { getAccount, removeAccount } from './utils/account';
+import getSecondsToExpire from './utils/jwt';
 
 export const ContextAccount = createContext<StoreProvider | null>(null);
 
@@ -16,6 +15,53 @@ const Store: React.FC = ({ children }) => {
   const [refreshToken, setRefreshToken] = useState('');
   const [cart, setCart] = useState<string[]>([]);
 
+  const [authorized, setAuthorized] = useState(true);
+
+  const startCountDown = useCallback(
+    (count) => {
+      const twoSeconds = 2000,
+        THIRTY_SECONDS = 30,
+        ONE_SECOND = 1;
+
+      if (count < ONE_SECOND) return 0;
+      let expireCount = count;
+
+      const secondsToExpireInterval: any = setInterval(() => {
+        expireCount--;
+
+        if (expireCount < THIRTY_SECONDS) {
+          setAuthorized(() => false);
+          clearInterval(secondsToExpireInterval);
+        } else if (expireCount < ONE_SECOND) {
+        }
+      }, twoSeconds);
+
+      return secondsToExpireInterval;
+    },
+    [token]
+  );
+
+  const loginByStorage = () => {
+    const { account, token, refreshToken } = getAccount();
+
+    const secondsToExpire = getSecondsToExpire(token);
+    const countDownID: any = startCountDown(secondsToExpire);
+
+    if (countDownID) {
+      setRefreshToken(refreshToken);
+      setAccount(account);
+      setToken(token);
+      setAuthorized(true);
+
+      return () => clearInterval(countDownID);
+    } else if (token) {
+      removeAccount();
+      setAuthorized(false);
+    }
+  };
+
+  useEffect(loginByStorage, [token]);
+
   return (
     <ContextAccount.Provider
       value={{
@@ -27,6 +73,7 @@ const Store: React.FC = ({ children }) => {
         setRefreshToken,
         cart,
         setCart,
+        authorized,
       }}
     >
       {children}

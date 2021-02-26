23901a997eec0af4e0bf9155b9cddb54550f607c
Nadno <eunadno@gmail.com>
2021-02-26 20:51:23 -0300

    created theme style module

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    white: '#f5f5f5',
+    primary: '#d64040',
+    secondary: '#b06d6d',
+    title: '#303030',
+    text: '#404040',
+    shadow: '#6d7bb025',
+  },
+};

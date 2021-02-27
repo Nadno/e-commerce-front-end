f865597d01c967034eb525fdb555c927e9fde414
Nadno <eunadno@gmail.com>
2021-02-27 16:50:59 -0300

    updated storeAccount argument type and getAccount return type

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -1,3 +1,4 @@
+import { CookieStore} from '../interfaces/hooks';
 import { setCookie, getCookie, removeCookie } from './storage';
 
 const expires = new Date();
@@ -9,14 +10,15 @@ export const COOKIE_ACCOUNT = 'acc';
 export const COOKIE_TOKEN = 'tk';
 export const COOKIE_REFRESH_TOKEN = 'rtk';
 
-export const storeAccount = (
-  account: object,
-  token: string,
-  refreshToken: string
-) => {
-  setCookie(COOKIE_ACCOUNT, account, options);
-  setCookie(COOKIE_TOKEN, token, options);
-  setCookie(COOKIE_REFRESH_TOKEN, refreshToken, options);
+
+export const storeAccount = ({
+  account,
+  token,
+  refreshToken,
+}: Partial<CookieStore>) => {
+  if (account) setCookie(COOKIE_ACCOUNT, account, options);
+  if (token) setCookie(COOKIE_TOKEN, token, options);
+  if (refreshToken) setCookie(COOKIE_REFRESH_TOKEN, refreshToken, options);
 };
 
 export const removeAccount = () => {
@@ -25,7 +27,7 @@ export const removeAccount = () => {
   removeCookie(COOKIE_REFRESH_TOKEN);
 };
 
-export const getAccount = () => ({
+export const getAccount = (): CookieStore => ({
   account: getCookie(COOKIE_ACCOUNT),
   token: getCookie(COOKIE_TOKEN),
   refreshToken: getCookie(COOKIE_REFRESH_TOKEN),

40f73218b2fc766b764a23d3400b944b26ccd461
Nadno <eunadno@gmail.com>
2021-02-28 11:37:04 -0300

    adjustments FormData generics types, and added Props types

--- src/HOC/form.tsx
+++ src/HOC/form.tsx
@@ -1,27 +1,25 @@
 import React, { ChangeEvent, useCallback, useState } from 'react';
-import FormProps from '../interfaces/form';
 
-export default function FormData<T extends { [x: string]: any }>(
-  FormComponent: React.FC<FormProps<T>>,
-  initialData: T
-): React.FC<FormProps<T>> {
-  return function FormContext() {
+export default function FormData<Props, Data extends Record<string, any>>(
+  FormComponent: WrappedComponent<Data, Props>,
+  initialData: Data
+) {
+  const Wrapper: React.FC<Props> = props => {
     const [data, setData] = useState(initialData);
 
-    const handleChange = useCallback(
-      (e: ChangeEvent) => {
-        const { name, value } = e.target as HTMLInputElement;
-      
-        if (typeof data[name] !== 'undefined') {
-          setData(prev => ({
-            ...prev,
-            [name]: value,
-          }));
-        }
-      },
-      []
-    );
+    const handleChange = useCallback((e: ChangeEvent) => {
+      const { name, value } = e.target as HTMLInputElement;
 
-    return <FormComponent data={data} handleChange={handleChange} />;
+      if (typeof data[name] !== 'undefined') {
+        setData(prev => ({
+          ...prev,
+          [name]: value,
+        }));
+      }
+    }, []);
+
+    return <FormComponent data={data} handleChange={handleChange} {...props} />;
   };
+
+  return Wrapper;
 }

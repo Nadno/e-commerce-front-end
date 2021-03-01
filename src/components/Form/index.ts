d4f9d61223d256ab6bda36dc5630badda513a7e5
Nadno <eunadno@gmail.com>
2021-03-01 15:57:07 -0300

    style, now Form extends Container as a form

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,19 +1,10 @@
 import styled from 'styled-components';
+import Container from '../Container/style';
 
-const Form: any = styled.form`
+const Form: any = styled(Container).attrs(() => ({ as: 'form' }))`
   height: 100%;
   min-height: 100vh;
-  padding: 4rem 2rem;
-
-  display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
-
-  background-color: ${({ theme }) => theme.colors.white};
-  box-shadow: 0 0 16px 4px ${({ theme }) => theme.colors.white};
-  border-top: 4px solid ${({ theme }) => theme.colors.secondary};
-  border-bottom: 4px solid ${({ theme }) => theme.colors.secondary};
 
   @media screen and (min-width: 450px) {
     .input-block {
@@ -44,7 +35,7 @@ Form.Fieldset = styled.fieldset`
   }
 `;
 
-Form.Submit = styled.button.attrs((_) => ({ type: 'submit' }))`
+Form.Submit = styled.button.attrs(() => ({ type: 'submit' }))`
   cursor: pointer;
   width: 100%;
   height: 4rem;

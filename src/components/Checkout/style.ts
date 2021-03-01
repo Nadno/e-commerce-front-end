4d4188bedb5f3d78065ba0608c48d4bd20379db6
Nadno <eunadno@gmail.com>
2021-03-01 15:58:22 -0300

    style, moved Checkout style to this module

---
+++ src/components/Checkout/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import Form from '../Form';
+
+export const Section = styled.section`
+  margin: 4rem 0;
+
+  @media screen and (min-width: 700px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+export const CheckoutForm = styled(Form)`
+  min-height: 100%;
+  justify-content: flex-start;
+`;

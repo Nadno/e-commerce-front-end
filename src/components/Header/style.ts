76cce95b52a328569870b2bc64c0afa5e23ba593
Nadno <eunadno@gmail.com>
2021-03-02 16:07:19 -0300

    style, list class changed to display grid and has width 100%

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -3,22 +3,23 @@ import Container from '../Container/style';
 
 const StyledHeader = styled(Container)`
   width: 100%;
-  height: 5rem;
+  height: 5.5rem;
   padding: 0;
 
   border-top: none;
 
   nav {
     height: 100%;
+    width: 100%;
   }
+  
 
   .list {
-    display: block;
-    height: 100%;
+    height: inherit;
+    width: inherit;
     list-style: none;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
     font-size: 2rem;
   }
 `;

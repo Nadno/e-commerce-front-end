009d99a1e33a9d75155b7457dce45269ecff982f
Nadno <eunadno@gmail.com>
2021-03-03 22:53:34 -0300

    util error, created errors to form, and created getInputError

---
+++ src/utils/getError.ts
@@ -0,0 +1,15 @@
+import errors from './errors.json';
+
+type InputError = { name: string; error: string };
+
+export function getInputError({ name, error }: InputError) {
+  const formErrors = errors.form as any;
+
+  if (formErrors[name] != null) {
+    return formErrors[name][error];
+  } else {
+    return formErrors.default[error]
+      ? formErrors.default[error]
+      : 'Ops, algo não está certo';
+  }
+}

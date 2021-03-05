1d41a6f3c204438c80e9c1f833b0049ad6f3e0b5
Nadno <eunadno@gmail.com>
2021-03-05 15:33:25 -0300

    validate has differents methods to differents forms

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,15 +1,17 @@
 import { getInputError } from './getError';
-import { notNull, validPattern } from './validations';
+import { isEqual, notNull, validPattern } from './validations';
 
 const patternFor: Record<string, RegExp> = {
   email: /^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/,
   password: /^[a-zA-Z-0-9]{6,30}$/,
-  phone: /\d{2}\s\9\d{4}\-\d{4}/,
+  tel: /^[1-9]{2} (?:[2-8]|9[1-9])[0-9]{3}[0-9]{4}$/,
 };
 
 const ERROR = {
   INVALID: 'invalid',
   EMPTY: 'empty',
+  DEPENDENT_EMPTY: 'dependentEmpty',
+  NOT_EQUAL: 'notEqual',
 };
 
 function validTests(name: string, tests: any) {
@@ -22,28 +24,58 @@ function validTests(name: string, tests: any) {
   return '';
 }
 
-function defaultValidation(name: string, value: any) {
-  const tests: any = [[notNull(value), ERROR.EMPTY]];
+function setDefaultValidation(optionalInputs: string[] = []) {
+  return function defaultValidation(name: string, data: any) {
+    const value = data[name];
 
-  if (patternFor[name])
-    tests.push([validPattern(patternFor[name], value), ERROR.INVALID]);
+    if (!optionalInputs.includes(name)) {
+      const tests: any = [[notNull(value), ERROR.EMPTY]];
 
-  return validTests(name, tests);
+      if (patternFor[name])
+        tests.push([validPattern(patternFor[name], value), ERROR.INVALID]);
+      
+      return validTests(name, tests);
+    }
+
+    return '';
+  };
 }
+const validationMethods: Record<string, any> = {
+  'sign-in': {
+    default: setDefaultValidation(),
+  },
+  'sign-up': {
+    default: setDefaultValidation(['avatar']),
 
-function setValidation(validation: any):
-  (name: string, value: any) => string {
-  return function validate(name: string, value: any) {
+    confirmPassword(name: string, data: any) {
+      const tests = [
+        [notNull(data.password), ERROR.DEPENDENT_EMPTY],
+        [notNull(data.confirmPassword), ERROR.EMPTY],
+        [isEqual(data.password, data.confirmPassword), ERROR.NOT_EQUAL],
+      ];
+  
+      return validTests(name, tests);
+    },
+  },
+};
+
+function setValidation(
+  validationName: string
+): (name: string, data: any) => string {
+  const validation = validationMethods[validationName];
+
+  return function validate(name, data) {
     if (validation[name] != null) {
-      return validation[name](name, value);
-    } else if (typeof value == 'string') {
-      return defaultValidation(name, value);
+      return validation[name](name, data);
+    } else {
+      return validation.default(name, data);
     }
   };
 }
-const validation: Record<string, Function> = {};
+
 const validate: any = {
-  'sign-in': setValidation(validation['sign-in'] || {}),
+  'sign-in': setValidation('sign-in'),
+  'sign-up': setValidation('sign-up'),
 };
 
 export default validate;

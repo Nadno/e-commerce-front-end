1eba6fca97ffd253c04c68fa7460c1f992b8bd36
Nadno <eunadno@gmail.com>
2021-03-05 15:43:39 -0300

    componet, adjustments to get a response from the return of apiGet

--- src/components/SearchSection/index.tsx
+++ src/components/SearchSection/index.tsx
@@ -1,12 +1,17 @@
-import React, { ChangeEvent, FormEvent, memo, useCallback, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FormEvent,
+  memo,
+  useCallback,
+  useState,
+} from 'react';
 
 import Link from '../Link';
+import Container from '../Container';
 import { apiGet } from '../../utils/api';
 
-import { Input } from '../Input';
-
+import { Input } from '../Input/style';
 import * as Search from './style';
-import Container from '../Container';
 
 interface Props {
   categories: string[];
@@ -28,9 +33,10 @@ const SearchSection: React.FC<Props> = ({ setProducts, categories }) => {
     async (e: FormEvent) => {
       e.preventDefault();
 
-      const products = await apiGet(`/product/name?value=${productName}`).then(
-        ({ data }) => data.products || data.product
-      );
+      const { response } = apiGet(`/product/name?value=${productName}`);
+      const products = await response
+        .then(({ data }) => data.products || data.product);
+
       setProducts(() => products);
     },
     [productName]
@@ -52,11 +58,12 @@ const SearchSection: React.FC<Props> = ({ setProducts, categories }) => {
         </form>
 
         <ul className="categories">
-          {categories.length > 0 && categories.map((cat) => (
-            <li key={cat}>
-              <Link href={`/products/category/${cat}`}>{cat}</Link>
-            </li>
-          ))}
+          {categories.length > 0 &&
+            categories.map(cat => (
+              <li key={cat}>
+                <Link href={`/products/category/${cat}`}>{cat}</Link>
+              </li>
+            ))}
         </ul>
       </Container>
     </Search.Section>

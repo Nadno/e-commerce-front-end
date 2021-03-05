f60f39803a3ea5a6b6bf7cf420819ce3e765dad2
Nadno <eunadno@gmail.com>
2021-03-05 15:22:27 -0300

    util api, fixed request error generated by the same signal token

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,16 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, CancelTokenSource } from 'axios';
 
 import { getCookie } from './storage';
 import { COOKIE_TOKEN } from './account';
 
-const signal = axios.CancelToken.source();
-
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  cancelToken: signal.token,
+  baseURL: 'http://192.168.1.2:3333',
 });
 
 type ApiObject = { cancelRequest: () => void; response: Promise<AxiosResponse> };
 
-const cancelRequest = () => signal.cancel("canceled");
+const cancel = (signal: CancelTokenSource) => 
+  () => signal.cancel('canceled');
 
 const getHeaders = () => {
   const token = getCookie(COOKIE_TOKEN);
@@ -35,22 +33,26 @@ export const apiRefreshToken = (refreshToken: string) => {
 };
 
 export const apiPost = (path: string, data = {}): ApiObject => {
+  const signal = axios.CancelToken.source();
   const options = {
     headers: getHeaders(),
+    cancelToken: signal.token,
   };
 
   return {
-    cancelRequest,
+    cancelRequest: cancel(signal),
     response: api.post(path, data, options),
   };
 };
 
 export const apiGet = (path: string): ApiObject => {
+  const signal = axios.CancelToken.source();
   const options = {
     headers: getHeaders(),
+    cancelToken: signal.token,
   };
   return {
-    cancelRequest,
+    cancelRequest: cancel(signal),
     response: api.get(path, options),
   };
 };

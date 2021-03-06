07210c1aef4e02861c9af96df2a7b5f528067f9e
Nadno <eunadno@gmail.com>
2021-03-06 19:48:52 -0300

    implemented credit-card validation to CheckoutForm

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import FormData, { WrappedComponent } from '../../HOC/form';
 
 import Form from '../Form';
-import DefaultInput from '../Input';
-
-import { CartProps } from '../Cart';
-import { Section, CheckoutForm } from './style';
+import { Input } from '../Input';
+import Select from '../Select';
 import OrderTable from '../OrderTable';
+import { Section, CheckoutForm } from './style';
+
+import useAccount from '../../hooks/useAccount';
+import { apiPost } from '../../utils/api';
+import handleRequest from '../../utils/handleRequests';
+import validate from '../../utils/validation/validate';
+import { CartProps } from '../Cart';
 
 const INITIAL_DATA = {
   cardNumber: '',
+  cardType: 'VS',
   cardOwner: '',
   cardValidate: '',
   cardSecurityCode: '',
@@ -22,55 +28,97 @@ interface Props extends Pick<CartProps, 'products'> {
 
 const Checkout: WrappedComponent<CheckoutData, Props> = ({
   data,
+  inputError,
+  invalid,
   handleChange,
   finalPrice,
   products,
 }) => {
+  const { account } = useAccount();
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+
+      const formattedProducts = Object.entries(products).map(
+        ([, product]) => product
+      );
+
+      const { response } = apiPost('/order/create', {
+        id: account.id,
+        products: formattedProducts,
+      });
+
+      response.then(console.log).catch(handleRequest(console.log));
+    },
+    [data, products]
+  );
+
   return (
     <Section>
       <OrderTable products={Object.entries(products)} finalPrice={finalPrice} />
 
-      <CheckoutForm>
+      <CheckoutForm onSubmit={handleSubmit}>
         <h2 className="title">Cartão</h2>
         <Form.Fieldset>
-          <DefaultInput
-            id="card-number"
-            name="cardNumber"
-            value={data.cardNumber}
-            label="Número do cartão"
-            placeholder="XXX XXX XXX XXXX"
-            handleChange={handleChange}
-          />
-          <DefaultInput
+          <div className="input-block">
+            <Input
+              id="card-number"
+              name="cardNumber"
+              error={inputError.cardNumber}
+              value={data.cardNumber}
+              label="Número do cartão"
+              placeholder="Apenas números"
+              onChange={handleChange}
+            />
+            <Select
+              id="card-type"
+              name="cardType"
+              value={data.cardType}
+              error={inputError.cardType}
+              label="Cartão"
+              onChange={handleChange}
+              options={[
+                { value: 'Visa', abbr: 'VS' },
+                { value: 'Mastercard', abbr: 'MS' },
+              ]}
+            />
+          </div>
+
+          <Input
             id="card-owner"
             name="cardOwner"
             value={data.cardOwner}
+            error={inputError.cardOwner}
             label="Nome"
             placeholder="Nome, presente no cartão"
-            handleChange={handleChange}
+            onChange={handleChange}
           />
 
-          <DefaultInput
+          <Input
             type="month"
             id="card-validate"
             name="cardValidate"
             value={data.cardValidate}
+            error={inputError.cardValidate}
             label="Validade"
             placeholder="MM/AA"
-            handleChange={handleChange}
+            onChange={handleChange}
           />
-          <DefaultInput
+          <Input
             id="card-security-code"
             name="cardSecurityCode"
             value={data.cardSecurityCode}
+            error={inputError.cardSecurityCode}
             label="Código de segurança"
             placeholder="123"
-            handleChange={handleChange}
+            onChange={handleChange}
           />
+          <Form.SubmitPrimary disabled={invalid}>Comprar</Form.SubmitPrimary>
         </Form.Fieldset>
       </CheckoutForm>
     </Section>
   );
 };
 
-export default FormData(Checkout, INITIAL_DATA);
+export default FormData(Checkout, INITIAL_DATA, validate['credit-card']);

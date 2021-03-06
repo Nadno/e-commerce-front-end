03447f4cd46c7bffca51c35371a3b27385369950
Nadno <eunadno@gmail.com>
2021-03-06 19:52:24 -0300

    created less/bigger OrEqualThan utils validations functions

--- src/utils/validation/validations.ts
+++ src/utils/validation/validations.ts
@@ -1,6 +1,16 @@
 export const validPattern = (pattern: RegExp, value: string) => (): boolean =>
   new RegExp(pattern).test(value);
 
+export const lessOrEqualThan = (
+  firstValue: number,
+  secondValue: number
+) => (): boolean => firstValue <= secondValue;
+
+export const biggerOrEqualThan = (
+  firstValue: number,
+  secondValue: number
+) => (): boolean => firstValue >= secondValue;
+
 export const isEqual = (
   firstValue: string,
   secondValue: string

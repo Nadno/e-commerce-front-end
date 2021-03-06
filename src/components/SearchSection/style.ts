5c9505d1904c8f8d4101ccff337785b6618fd6ac
Nadno <eunadno@gmail.com>
2021-03-06 20:02:31 -0300

    Styled Submit component removed; button[type=submit] edited from the search-bar; changed search max-width

--- src/components/SearchSection/style.ts
+++ src/components/SearchSection/style.ts
@@ -1,15 +1,24 @@
 import styled from 'styled-components';
 
-import { Input } from '../Input/style';
-
 const Section = styled.section`
   .search-bar {
+    width: 100%;
+    height: 4rem;
     display: flex;
     align-items: center;
     justify-content: center;
 
     #search {
-      max-width: 50rem;
+      max-width: 32rem;
+      margin-right: 1rem;
+    }
+
+    button[type="submit"] {
+      width: 8.2rem;
+      height: 100%;
+      margin: 0;
+
+      border-radius: 0.2em;
     }
   }
 
@@ -26,16 +35,4 @@ const Section = styled.section`
   }
 `;
 
-const Submit = styled(Input)`
-  width: initial;
-  cursor: pointer;
-
-  transition: background-color 0.5s ease;
-
-  &:hover {
-    color: ${({ theme }) => theme.colors.white};
-    background-color: ${({ theme }) => theme.colors.secondary};
-  }
-`;
-
-export { Section, Submit };
+export { Section };

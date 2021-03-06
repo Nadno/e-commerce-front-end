a8b44c8dc23db19ea92fb8d81de30e18cc172295
Nadno <eunadno@gmail.com>
2021-03-06 20:10:11 -0300

    ListItem implements Animation props, to dynamic durations or delays on animation

--- src/components/Product/style.ts
+++ src/components/Product/style.ts
@@ -1,18 +1,24 @@
 import styled from 'styled-components';
+import Animation from '../../types/animation';
 
-const ListItem = styled.li`
+const ListItem = styled.li<Animation>`
+  animation: show-product
+    ${({ duration, delay, fill, cubic }) =>
+      `${duration} ${delay || 0} ${fill || ''} ${cubic || ''}`};
+
   width: 100%;
-
   padding: 1.5rem;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  opacity: 0;
 
-  box-shadow: 0 -4px 0 ${({ theme }) => theme.colors.primary},
+  box-shadow: 0 -4px 0 ${({ theme }) => theme.colors.secondary},
     0 0 16px 8px ${({ theme }) => theme.colors.shadow};
   background-color: white;
 
+  transform: translateY(-6.4rem);
   gap: 2.5rem;
 
   .img {
@@ -27,10 +33,17 @@ const ListItem = styled.li`
       width: 8rem;
     }
   }
+
+  @keyframes show-product {
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;
 
 export const ProductContent = styled.div.attrs(() => ({
-  className: 'content'
+  className: 'content',
 }))`
   width: 100%;
   height: 100%;
@@ -44,15 +57,10 @@ export const ProductContent = styled.div.attrs(() => ({
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-  }
-
-  .title {
-    font-size: 2rem;
-    margin-top: 0;
+    gap: inherit;
   }
 
   .description {
-    display: block;
     font-size: 1.6rem;
   }
 

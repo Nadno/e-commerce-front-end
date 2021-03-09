b77fa40aaab85fe92a1285aef609a001a22bbadc
Nadno <eunadno@gmail.com>
2021-03-08 23:30:56 -0300

    divide the Modal in two types, Action and Warn

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,40 +3,57 @@ import Button from '../Button';
 import { ModalContainer, Background } from './style';
 
 interface Props {
+  className: string;
   message: string;
   isOpen: boolean;
-  cancelText?: string;
+}
+
+interface WarnProps extends Omit<Props, 'type' | 'className'> {
   okText?: string;
   handleOk(e: MouseEvent): void;
+}
+
+interface ActionProps extends WarnProps {
+  cancelText?: string;
   handleCancel(e: MouseEvent): void;
 }
 
-const Modal: React.FC<Props> = ({
-  isOpen,
-  message,
-  okText,
-  cancelText,
-  handleOk,
-  handleCancel,
-}) => (
+const Modal: React.FC<Props> = ({ isOpen, message, className, children }) => (
   <>
     {isOpen ? (
       <Background>
-        <ModalContainer>
+        <ModalContainer className={className}>
           <span className="message">{message}</span>
           <hr />
-          <div className="buttons">
-            <Button.Primary onClick={handleOk}>
-              {okText ? okText : 'OK'}
-            </Button.Primary>
-            <Button.Primary onClick={handleCancel}>
-              {cancelText ? cancelText : 'Cancelar'}
-            </Button.Primary>
-          </div>
+          <div className="buttons">{children}</div>
         </ModalContainer>
       </Background>
     ) : null}
   </>
 );
 
-export default Modal;
+const Action: React.FC<ActionProps> = ({
+  okText,
+  cancelText,
+  handleOk,
+  handleCancel,
+  ...props
+}) => (
+  <Modal {...props} className="action">
+    <Button.Primary onClick={handleOk}>{okText ? okText : 'OK'}</Button.Primary>
+    <Button.Primary onClick={handleCancel}>
+      {cancelText ? cancelText : 'Cancelar'}
+    </Button.Primary>
+  </Modal>
+);
+
+const Warn: React.FC<WarnProps> = ({ okText, handleOk, ...props }) => (
+  <Modal {...props} className="warn">
+    <Button.Primary onClick={handleOk}>{okText ? okText : 'OK'}</Button.Primary>
+  </Modal>
+);
+
+export default {
+  Action,
+  Warn,
+};

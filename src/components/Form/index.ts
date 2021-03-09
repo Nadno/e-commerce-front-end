14467d4455082ce939b210744a0468ae7690642b
Nadno <eunadno@gmail.com>
2021-03-08 23:20:30 -0300

    change exports from Form component

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -2,22 +2,30 @@ import styled from 'styled-components';
 import Button from '../Button';
 import Container from '../Container/style';
 
-const Form: any = styled(Container).attrs(() => ({ as: 'form' }))`
+const Form = styled(Container).attrs(props => ({ ...props, as: 'form', }))`
+  width: 100%;
+  height: 100%;
+  max-width: 750px;
+  min-height: 100vh;
   justify-content: center;
 
   .input-block {
     display: grid;
     grid-template: 1fr 1fr / 1fr;
     gap: 1.5rem;
+  }
 
-    @media screen and (min-width: 450px) {
+  @media screen and (min-width: 550px) {
+    margin: 0 auto;
+
+    .input-block {
       grid-template: 1fr / 1fr 1fr;
       align-items: flex-end;
     }
   }
 `;
 
-Form.Fieldset = styled.fieldset`
+const Fieldset = styled.fieldset`
   max-width: 50rem;
   width: 100%;
   padding: 0 2rem;
@@ -27,9 +35,12 @@ Form.Fieldset = styled.fieldset`
   margin-bottom: 4rem;
 
   legend {
+    width: 100%;
+    text-align: center;
     font-size: 2rem;
     font-weight: bold;
-    text-align: center;
+    padding-bottom: 0.2em;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
   }
 
   & > div {
@@ -37,7 +48,7 @@ Form.Fieldset = styled.fieldset`
   }
 `;
 
-Form.SubmitPrimary = styled(Button.Primary).attrs(props => ({
+const PrimarySubmit = styled(Button.Primary).attrs(props => ({
   type: 'submit',
   ...props,
 }))`
@@ -45,9 +56,9 @@ Form.SubmitPrimary = styled(Button.Primary).attrs(props => ({
   margin-top: 3rem;
 `;
 
-Form.SubmitSecondary = styled(Button.Secondary).attrs(props => ({
+const SecondarySubmit = styled(Button.Secondary).attrs(props => ({
   type: 'submit',
   ...props,
 }))``;
 
-export default Form;
+export { Form, Fieldset, PrimarySubmit, SecondarySubmit };

a23c8f7b365e23ddd6d4988cac6d29ae3a135cd2
Nadno <eunadno@gmail.com>
2021-03-11 23:28:51 -0300

    re-style header and its links

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -3,6 +3,7 @@ import Container from '../Container/style';
 
 const StyledHeader = styled(Container)`
   width: 100%;
+  height: 8rem;
   padding: 0.8rem;
 
   border-top: none;
@@ -12,17 +13,22 @@ const StyledHeader = styled(Container)`
     width: 100%;
   }
 
-  a {
-    height: 3.5rem;
-    padding: 0.4em;
+  a, li {
+    height: 100%;
   }
+
+  a {
+    padding: 1em;
+  }
+
 
   .list {
     height: inherit;
     width: inherit;
     list-style: none;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
     font-size: 1.8rem;
   }
 `;

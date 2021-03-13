b3f64fcfcfe8faa79c598414f0c10421be5430a1
Nadno <eunadno@gmail.com>
2021-03-13 15:12:33 -0300

    implement GridContainer around SignUp screen

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,3 +1,11 @@
+import React from 'react';
 import SignUp from '../screen/sign-up';
+import { GridContainer } from '../components/Container/style';
 
-export default SignUp;
+const SignUpPage: React.FC = () => (
+  <GridContainer>
+    <SignUp />
+  </GridContainer>
+);
+
+export default SignUpPage;

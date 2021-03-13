191f863192b3bf9aaeac2bec844dbab9dd2fc72c
Nadno <eunadno@gmail.com>
2021-03-13 14:19:25 -0300

    Extract and import Styled Buttons, and bring Submit button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,108 +1,42 @@
 import React from 'react';
-import NextLink, { LinkProps } from 'next/link';
 import router from 'next/router';
+import NextLink, { LinkProps } from 'next/link';
 
-import styled from 'styled-components';
+import { PrimaryButton, SecondaryButton } from './style';
 
-const StyledButton = styled.button`
-  cursor: pointer;
-  height: 4.5rem;
-  padding: 0.4em 1em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const Submit: React.FC<{ type?: 'primary' | 'secondary' }> = ({
+  type = 'primary',
+  children,
+}) => {
+  const SubmitButton = type === 'primary' ? PrimaryButton : SecondaryButton;
 
-  font-size: 1.6rem;
-  font-weight: bold;
-  transition: transform 0.2s ease;
-
-  &:active {
-    transform: scale(0.95) !important;
-  }
-
-  &:disabled {
-    cursor: not-allowed;
-  }
-`;
-
-const Primary = styled(StyledButton)`
-  width: 100%;
-  border: none;
-  color: ${({ theme }) => theme.colors.white};
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  &:hover {
-    transform: scale(1.05);
-  }
-
-  &:focus {
-    outline: none;
-    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.background},
-      0 0 0 4px ${({ theme }) => theme.colors.primary};
-  }
-`;
-
-const Secondary = styled(StyledButton)`
-  position: relative;
-  transform-style: preserve-3d;
-  overflow: hidden;
-  color: ${({ theme }) => theme.colors.secondary};
-  border: 2px solid ${({ theme }) => theme.colors.secondary};
-
-  &::before {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: ${({ theme }) => theme.colors.secondary};
-
-    z-index: -1;
-    transform: translateY(100%);
-    transition: transform 250ms ease;
-  }
-
-  &:hover {
-    color: ${({ theme }) => theme.colors.white};
-
-    &::before {
-      transform: translateX(0);
-    }
-  }
-
-  &:focus {
-    outline: none;
-    color: ${({ theme }) => theme.colors.white};
-    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.background},
-      0 0 0 4px ${({ theme }) => theme.colors.secondary};
-
-    &::before {
-      transform: translateX(0);
-    }
-  }
-`;
+  return (
+    <SubmitButton type="submit">
+      {children}
+    </SubmitButton>
+  );
+};
 
 const Link: React.FC<LinkProps> = ({ children, href, ...props }) => {
   return (
     <NextLink href={href} passHref>
-      <Secondary {...props} as="a">
+      <SecondaryButton {...props} as="a">
         {children}
-      </Secondary>
+      </SecondaryButton>
     </NextLink>
   );
 };
 
-const Back: React.FC<{
-  className?: string;
-}> = ({ className = '' }) => {
+const ButtonBack: React.FC = () => {
   return (
-    <Secondary
+    <SecondaryButton
       style={{ width: '10rem' }}
       onClick={() => router.back()}
-      className={className}
+      className="back"
     >
       Voltar
-    </Secondary>
+    </SecondaryButton>
   );
 };
 
-export default { Primary, Secondary, Link, Back };
+export { Link, ButtonBack, Submit };

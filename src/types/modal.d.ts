e991456996eedfbf1f8ecb4d120a7eb1376e9907
Nadno <eunadno@gmail.com>
2021-04-19 13:53:06 -0300

    unimplement react modal provider, and implemente modal with react portals

--- src/types/modal.d.ts
+++ src/types/modal.d.ts
@@ -1,29 +1,22 @@
 import { Dispatch, SetStateAction } from 'react';
 
-export type Actions = { okAction?: () => void; cancelAction?: () => void };
+export type Actions = { handleConfirm?: () => void; handleCancel?: () => void };
 export type Buttons = {
-  okButtonText?: string;
-  cancelButtonText?: string;
+  confirmText?: string;
+  cancelText?: string;
 };
 export type ModalTypes = 'Warn' | 'Action';
 export type OpenModal = () => void;
 export type SetActions = ({}: Actions) => void;
 export type SetButtons = ({}: Buttons) => void;
 
-interface CreateModalParams extends Actions {
+interface CreateModalParams extends Actions, Buttons {
   message: string;
 }
-interface CreateModalParams extends Buttons {}
 
-export type CreateModalMethods = ({
-  message,
-  okAction,
-  okButtonText,
-  cancelAction,
-  cancelButtonText,
-}: CreateModalParams) => void;
+export type CreateModalMethods = ({}: CreateModalParams) => void;
 
-export type CreateModal = Record<string, CreateModalMethods>;
+export type SetModalAs = Record<string, CreateModalMethods>;
 
 export type Provider = {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
